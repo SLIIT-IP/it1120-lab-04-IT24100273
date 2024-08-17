cd6a1a920d88babce749d00ad8ecd4e41a5f094e
IT24100273 <it24100273@my.sliit.lk>
2024-08-17 13:13:23 +0530

    Add files via upload

---
+++ Q1/IT24100273Lab4Q1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class IT24100273Lab4Q1{
+
+    public static void main(String[] args){
+     
+         Scanner scanner = new Scanner(System.in);
+         System.out.print("Enter a number : ");
+
+         int num = scanner.nextInt();
+         
+         if (num > 0)
+         { 
+            System.out.println("Positive");
+
+         }
+ 
+         else
+         {
+            System.out.println("Negative");
+         } 
+                         
+     }
+
+}       
